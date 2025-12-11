c717aca902538d39512eb0c7df859039b9328853
fallprotect2 <john.noriega@fallprotect.com>
2025-12-11 15:54:38 -0500

    Update vite.config.ts
    Update base path for EZ-Series Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ez-ladder-config/', // repo name
+  base: '/ez-series-config/', // repo name
   plugins: [react()]
 })
